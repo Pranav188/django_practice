ab50850c554016a13b0e4740f6ac9bfbdb38f08e
Pranav188 <146744129+Pranav188@users.noreply.github.com>
2025-06-05 16:50:03 +0530

    includes

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_page),
+    path('', views.home_page, name = "index"),
     path('<int:month>/', views.func_month_number),
     path('<str:month>/', views.func_month, name = 'pranav_website'),
 ]
